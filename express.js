444b251374c38977efccf32a3322d8a4eac62b0b
scottqueen-bixal <scott.queen@bixal.com>
2025-07-10 06:16:43 -0400

    set up nodemon

--- express.js
+++ express.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 const app = express();
-const port = 3000;
+const port = 8000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
